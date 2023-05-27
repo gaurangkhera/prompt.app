c248bf4c8768d4f19eb9d7b80fe3cb8b80d3871f
Gaurang <khera.gaurang@gmail.com>
2023-05-27 08:56:32 +0530

    a

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+
+
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      discover & share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> AI powered prompts</span>
+    </h1>
+    <p className='desc text-center'>
+      prompt.app is an open source AI prompting tool for the modern world to
+      discover, create and share creative prompts.
+    </p>
+
+  </section>
+);
+
+export default Home;
