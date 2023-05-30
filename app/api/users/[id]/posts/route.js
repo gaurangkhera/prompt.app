aedc7cb27a20a2b941187146ddc49524a6ef1a5f
Gaurang Khera <khera.gaurang@gmail.com>
2023-05-30 16:53:34 +0530

    Convert all db connecting functions into asynchronous

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -4,7 +4,7 @@ import { connectToDb } from "@/utils/db";
 export const GET = async (request, {params}) => {
     console.log(params.id)
     try{
-        connectToDb();
+        await connectToDb();
 
         let prompts = await Prompt.find({ creator: params.id }).populate('creator');
         return new Response(JSON.stringify(prompts), { status: 200})
